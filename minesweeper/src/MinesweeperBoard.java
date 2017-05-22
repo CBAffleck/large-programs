339e6751c4ef61ae091c1f4374d2d072a60a6e79
Campbell Affleck <cbaffleck@berkeley.edu>
2017-05-22 15:34:43 -0700

    Added comments and a couple TODO's

--- minesweeper/src/MinesweeperBoard.java
+++ minesweeper/src/MinesweeperBoard.java
@@ -15,7 +15,6 @@ public class MinesweeperBoard implements MouseListener {
     private final JPanel gui = new JPanel(new BorderLayout(3, 3));
     private JButton[][] boardTiles = new JButton[8][8];
     private JPanel board;
-    private JLabel message = new JLabel("The mines are planted!");
 
     public MinesweeperBoard() {
         startGui();
@@ -37,19 +36,20 @@ public class MinesweeperBoard implements MouseListener {
     }
 
     public void mouseReleased(MouseEvent e) {
-        int x = ((JButton) e.getSource()).getX()/48;
-        int y = ((JButton) e.getSource()).getY()/42;
-        if (e.getButton() == MouseEvent.BUTTON1) {
-            boardTiles[x][y].setEnabled(true);
+        int x = ((JButton) e.getSource()).getX()/48; //Gets x index of tile
+        int y = ((JButton) e.getSource()).getY()/42; //Gets y index of tile
+        if (e.getButton() == MouseEvent.BUTTON1) { //Checks if user left-clicks
+            boardTiles[x][y].setEnabled(true); //If it's a left click, the icon changes to the "touched" icon
+            // TODO: allow icon to change to numbered icon based on number of surrounding bombs, or change to blank icon if no bombs around it
             boardTiles[x][y].setIcon(new ImageIcon(new ImageIcon("Resources/touched_icon.png").getImage().getScaledInstance(44,38,Image.SCALE_SMOOTH), "touched"));
-        } else if (e.getButton() == MouseEvent.BUTTON3) {
-            if (((ImageIcon) boardTiles[x][y].getIcon()).getDescription().equals("touched")) {
+        } else if (e.getButton() == MouseEvent.BUTTON3) { //If it's a right click, do code for flagging and unflagging tile.
+            if (((ImageIcon) boardTiles[x][y].getIcon()).getDescription().equals("touched")) { //Checks if the tile is already touched, so it can't be flagged anymore
                 //do nothing
-            } else if (boardTiles[x][y].isEnabled()) {
-                boardTiles[x][y].setEnabled(false);
+            } else if (boardTiles[x][y].isEnabled()) { //If untouched, change icon to flag, letting the user remember mine location
+                boardTiles[x][y].setEnabled(false); //Disabled icon is the flag
                 boardTiles[x][y].setDisabledIcon(new ImageIcon(new ImageIcon("Resources/flagged_icon.png").getImage().getScaledInstance(44,38,Image.SCALE_SMOOTH), "flagged"));
             } else {
-                boardTiles[x][y].setEnabled(true);
+                boardTiles[x][y].setEnabled(true); //If the icon is a flag, this unflags the icon.
                 boardTiles[x][y].setIcon(new ImageIcon(new ImageIcon("Resources/untouched_icon.png").getImage().getScaledInstance(44,38,Image.SCALE_SMOOTH), "background"));
             }
         }
@@ -63,37 +63,38 @@ public class MinesweeperBoard implements MouseListener {
     public final void startGui() {
         // Sets up gui for the game.
         gui.setBorder(new EmptyBorder(5, 5, 5, 5));
-        JToolBar tools = new JToolBar();
+        JToolBar tools = new JToolBar(); //create toolbar to add a new game button above the game grid
         tools.setFloatable(false);
+        //Add button to toolbar
         gui.add(tools, BorderLayout.PAGE_START);
-        tools.add(new JButton("Main Menu"));
+        JButton newGame = new JButton("New Game");
+        newGame.addMouseListener(this);
+        tools.add(newGame);
         tools.addSeparator();
-        tools.add(new JButton("New Game"));
-        tools.addSeparator();
-        tools.add(new JButton("Reset"));
-        tools.addSeparator();
-        gui.add(new JLabel("Mines found: 0"));
-        tools.add(message);
+        //TODO: Write code for counter to update as time passes
+        JLabel counter = new JLabel("Time: 00:00"); //Shows time passed in minutes and seconds
+        tools.add(counter);
 
         board = new JPanel(new GridLayout(0, 8));
         board.setBackground(Color.decode("#fff2d3"));
         board.setBorder(new LineBorder(Color.black));
         gui.add(board);
 
-        //Adds button tiles to the board
+        //Creates JButton tiles for board
         Insets buttonMargin = new Insets(1,1,1,1);
         for (int i = 0; i < boardTiles.length; i++) {
             for (int j = 0; j < boardTiles[i].length; j++) {
                 JButton tile = new JButton();
                 tile.setMargin(buttonMargin);
-                tile.setEnabled(true);
+                tile.setEnabled(true); //sets default, untouched tile as "enabled"
                 tile.setIcon(new ImageIcon(new ImageIcon("Resources/untouched_icon.png").getImage().getScaledInstance(44,38,Image.SCALE_SMOOTH), "background"));
                 tile.setBackground(Color.decode("#74d1fc"));
-                tile.addMouseListener(this);
+                tile.addMouseListener(this); //adds mouseListener to the JButton tile
                 boardTiles[j][i] = tile;
             }
         }
 
+        //Adds all of the tiles to the board
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 board.add(boardTiles[j][i]);
@@ -119,11 +120,9 @@ public class MinesweeperBoard implements MouseListener {
                 frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
                 frame.setLocationByPlatform(true);
                 frame.setSize(400,400);
-                //frame.setBackground(Color.decode("#fff2d3"));
-                frame.setMinimumSize(frame.getSize());
+                frame.setMinimumSize(frame.getSize()); //User can't change window size when the min and max are the same
                 frame.setMaximumSize(frame.getSize());
                 frame.setVisible(true);
-                System.out.println();
             }
         };
         SwingUtilities.invokeLater(r);
