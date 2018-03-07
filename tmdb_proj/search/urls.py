b235c6ad6abf8198cf394ab3d5de372b87f12d18
Campbell Affleck <cbaffleck@berkeley.edu>
2018-03-06 22:57:29 -0800

    Added search functionality for movies and tv shows. Added pages to show the search results and then the info for the item the user selects. Added css file for search results page. Added links to each page that point back to the home page.

--- tmdb_proj/search/urls.py
+++ tmdb_proj/search/urls.py
@@ -6,4 +6,6 @@ app_name = 'search'
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('search_results/', views.search_results, name='search_results'),
+	path('<int:movie_id>/movie_info/', views.movie_info, name='movie_info'),
+	path('<int:tv_show_id>/tv_show_info/', views.tv_show_info, name='tv_show_info'),
 ]
--- tmdb_proj/search/views.py
+++ tmdb_proj/search/views.py
@@ -12,9 +12,11 @@ def index(request):
 
 def search_results(request):
 	if request.method == 'GET':
-		search_query = request.GET.get('search_box', None)
-		context = get_movie_title_id_dict(search_query)
-		return render(request, 'search/search_results.html', context)
+		search_query = request.GET.get('search_box', None).rstrip()
+		movie_titles_list = get_movie_title_id_dict(search_query)
+		tv_show_titles_list = get_tv_show_title_id(search_query)
+		titles_dict = {'movie_titles':movie_titles_list, 'tv_show_titles':tv_show_titles_list}
+		return render(request, 'search/search_results.html', titles_dict)
 
 #Takes the title the user put in and looks it up. If there's more than one movie with that title, then the user
 #is asked to select the correct one. The function returns the TMDB movie id.
@@ -25,8 +27,49 @@ def get_movie_title_id_dict(movie_title):
 	dict = response.json()
 
 	results = dict['results']
-	titles = {}
-	for d in results:
-		if d['title'].find(movie_title) == 0:
-			titles[d['title']] = [d['title'], d['id'], d['release_date']]
+	titles = [[d['title'], d['id'], d['release_date'], d['overview']] for d in results if d['title'].find(movie_title) == 0]
 	return titles
+
+#Takes the title the user put in and looks it up. If there's more than one tv show with that title, then the user
+#is asked to select the correct one. The function returns the TMDB tv show id.
+def get_tv_show_title_id(tv_show_title):
+	input_title_compressed = tv_show_title.replace(" ", "+")
+	url = "https://api.themoviedb.org/3/search/tv?api_key=70f9ef47636a6a60f21572480e7758e6&query=" + input_title_compressed
+	response = requests.get(url)
+	dict = response.json()
+
+	results = dict['results']
+	titles = [[d['name'], d['id'], d['first_air_date'], d['overview']] for d in results]
+	return titles
+
+def movie_info(request, movie_id):
+	info_list = get_movie_info(movie_id)
+	info_dict = {'movie_info':info_list}
+	return render(request, 'search/movie_info.html', info_dict)
+
+def tv_show_info(request, tv_show_id):
+	info_list = get_tv_show_info(tv_show_id)
+	info_dict = {'tv_show_info':info_list}
+	return render(request, 'search/tv_show_info.html', info_dict)
+
+#Gets the movie's info based on the id
+def get_movie_info(movie_id):
+	url = "http://api.themoviedb.org/3/movie/" + str(movie_id) + "?language=en-US&api_key=70f9ef47636a6a60f21572480e7758e6"
+	response = requests.get(url)
+	dict = response.json()
+
+	results = [dict['overview'], dict['release_date'], dict['genres'], dict['runtime'], dict['title']]
+	return results
+
+#Gets the tv show's info based on the id
+def get_tv_show_info(tv_show_id):
+	url = "http://api.themoviedb.org/3/tv/" + str(tv_show_id) + "?language=en-US&api_key=70f9ef47636a6a60f21572480e7758e6"
+	response = requests.get(url)
+	dict = response.json()
+
+	results = [dict['name'], dict['overview'], dict['first_air_date'], dict['number_of_seasons'], dict['number_of_episodes'], dict['status']]
+	return results
+
+
+
+
